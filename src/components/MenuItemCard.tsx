34018b28d955cecd1aef5fe9a50d3be89e71030f
duc.tran <duc.tran@tekos.net>
2024-11-04 23:08:41 +0700

    refactor

--- src/components/MenuItemCard.tsx
+++ src/components/MenuItemCard.tsx
@@ -20,44 +20,35 @@ const ProductOptionCard: React.FC<ProductOption> = ({
   description,
   batchSales,
 }) => {
-  // this will interact with the central store like redux
   const handleAddItem = (id: string) => {};
+
   return (
     <Box key={id} sx={{ px: 3, py: { xs: 2, md: 2.5 }, boxShadow: 4 }}>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
+      <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
         <Box
           onClick={() => handleAddItem(id)}
           sx={{
             display: "flex",
             cursor: "pointer",
             alignItems: "center",
-            justifyContent: "center",
             gap: 1,
           }}
         >
-          <AddShoppingCartIcon style={{ width: 16, height: 16 }} />
-
+          <AddShoppingCartIcon sx={{ width: 16, height: 16 }} />
           <Typography variant="body1" sx={{ fontWeight: "bold" }}>
             {label}
           </Typography>
         </Box>
-        <Typography variant="body1" sx={{ fontWeight: "bold", paddingLeft: 1 }}>
+        <Typography variant="body1" sx={{ fontWeight: "bold", pl: 1 }}>
           &#8364;{price.toFixed(2)}
         </Typography>
       </Box>
       {description && (
         <Tooltip arrow title={description}>
           <Typography
-            noWrap
-            sx={{ mt: 1, overflow: "hidden", textOverflow: "ellipsis" }}
             variant="body2"
             color="textSecondary"
+            sx={{ mt: 1, overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap" }}
           >
             {description}
           </Typography>
@@ -72,12 +63,12 @@ const ProductOptionCard: React.FC<ProductOption> = ({
             display: "inline-block",
             backgroundColor: "grey.300",
             p: 1,
-            lineHeight: "100%",
+            lineHeight: 1,
             borderRadius: 2,
           }}
         >
           {`${batchSales.quantity} x ${label} for `} &#8364;
-          {`${batchSales.batchPrice.toFixed(2)}`}
+          {batchSales.batchPrice.toFixed(2)}
         </Typography>
       )}
     </Box>
